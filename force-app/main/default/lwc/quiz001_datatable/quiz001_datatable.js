dcb6cb338993bf043e3cfd6dc1d3e0cf0fd9ba83
Rhea <rheagrace.sy@fox.com>
2022-06-02 19:01:19 -0700

    remove code

--- force-app/main/default/lwc/quiz001_datatable/quiz001_datatable.js
+++ force-app/main/default/lwc/quiz001_datatable/quiz001_datatable.js
@@ -1,24 +1,27 @@
 import { LightningElement, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-//import getRecords from '@salesforce/apex/Quiz001_Controller';
+/* PLEASE LEAVE COMMENTED 
+ * As part of our team, you'll be responsible for managing existing LWCs, and if applicable, create new ones.   
+ * For this quiz, you're tasking with completing a LWC. We've purposely excluded key features to test your working knowledge of LWC. 
+ * 
+ * OBJECTIVE:
+ * Display data using a simple datatable.
+ * 
+ * REQUIREMENTS:
+ * 1. Using imperative apex, retrieve data on page load.
+ * 2. In the event of a failure, catch and show error using toast message.
+ * */
 
 export default class Quiz001_datatable extends LightningElement {
     @track data = [];
-    columns = [
-        { label: 'Name', fieldName: 'Name', type: 'text'}
-    ];
+    columns = [];
 
     getData(){
         try{
-            // Add logic here to retrieve data from apex method
+
         }catch(error){
-            console.error(JSON.stringify(error));
-            // Add logic here to show on-screen toast message
-            this.dispatchEvent(new ShowToastEvent({
-                title: 'Error',
-                message: 'Failed'
-            }));
+            
         }
     }
 }
